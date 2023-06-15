f34a8c7528e2d0319c88b12516847690d9387f89
Will Weston <Williamweston81@gmail.com>
2023-06-15 17:08:25 -0500

    Weston (1) Manipulate Variables

--- if_statements.rb
+++ if_statements.rb
@@ -63,7 +63,7 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 2
+cups_of_flour = 5
 has_sauce = false
 
 if cups_of_flour == 1 && has_sauce == true 
