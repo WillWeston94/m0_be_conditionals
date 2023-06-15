d8ee4d06edb4408d30191f1f050f4c3ef8f3ed96
Will Weston <Williamweston81@gmail.com>
2023-06-15 17:53:14 -0500

    Weston(1) Initial Commit

---
+++ code_challenges_medium.rb
@@ -0,0 +1,10 @@
+good_driving_record = true
+age = 24
+
+if good_driving_record == true && age >= 24
+    puts "Discount on car rental granted"
+elsif good_driving_record == true || age >= 24
+    puts "No Discount"
+else good_driving_record == false || age < 24
+    puts "Not Eligible for Rental. Must have another person sign"
+end
